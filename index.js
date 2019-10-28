67ed6575210c53fe4483de1caa17debfb955e391
Jake Maldonado <jmaldonado@financeit.io>
2019-10-28 08:52:35 -0400

    add to be continued action

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
-/**
- *  @license
- * Epilepsy.js
- * License: MIT
- * Author: Joseph An
- * Version: 0.0.1 (2019/09/30)
- */
-
 (function() {
-  setInterval(function() {
-    var red = Math.floor(Math.random() * 255);
-    var blue = Math.floor(Math.random() * 255);
-    var green = Math.floor(Math.random() * 255);
-    var color = 'rgb(' + [red, green, blue].join(',') + ')';
+  const img = document.createElement("img");
 
-    var elems = document.querySelectorAll('*');
-    for (var i = 0, len = elems.length; i < len; i++) {
-      elems[i].style.backgroundColor = color;
-    }
+  img.style.position = 'fixed'
+  img.style.width = '20vw'
+  img.style.zIndex = '99'
+  img.style.bottom = '0'
+  img.style.float = 'left'
 
-  }, 100);
+  img.src = "https://img.pngio.com/jjba-to-be-continued-arrow-by-bahyyazidramadhan-on-deviantart-to-be-continued-arrow-png-600_338.png";
+  
+  const src = document.getElementsByTagName('body')[0];
+  
+  
+  const iFrameWrapper = document.createElement('div');
+  iFrameWrapper.style.display = "none"
+
+  iFrameWrapper.innerHTML  = '<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/137624004&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>'
+
+  setTimeout(() => {
+    src.appendChild(img);
+    src.style.filter = "sepia(80%)" 
+  }, 45000);
+
+  src.appendChild(iFrameWrapper);
 })()
